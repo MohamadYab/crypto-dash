c9e81261afe5b36bd25a2bd151581cbe49766dbc
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2025-07-24 02:49:47 +0100

    Added a LimitSelector component to control the per_page param to limit the number of returned items

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const API_FILTERS = "&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const response = await fetch(API_URL + API_FILTERS);
+        const response = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
         if (!response.ok) throw new Error('Failed to fetch data');
         const data = await response.json();
         console.log(data);
@@ -25,7 +26,7 @@ function App() {
       }
     }
     fetchCoins();
-  }, []);
+  }, [limit]);
   return (
     <div>
       <h1>Crypto Dash</h1>
@@ -35,11 +36,14 @@ function App() {
         ) : error ? (
           <div className="error">{error}</div>
         ) : (
-          <main className="grid">
-            {coins.map((coin) => (
-              <CoinCard key={coin.id} coin={coin} />
-            ))}
-          </main>
+          <>
+            <LimitSelector limit={limit} onChange={(e) => setLimit(e.target.value)} />
+            <main className="grid">
+              {coins.map((coin) => (
+                <CoinCard key={coin.id} coin={coin} />
+              ))}
+            </main>
+          </>
         )
       }
     </div>
